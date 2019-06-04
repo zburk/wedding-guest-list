53b85c4c5f2b4f54b79d84a8f2746aabb29cdba3
Zachary Burk <zachburk7@gmail.com>
2019-06-03 22:29:56 -0400

    Register service worker

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import AddGuest from './components/AddGuest';
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 
 class Header extends React.Component {
   render() {
